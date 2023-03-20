f0002127e70c5f4cc3297d43fbb738a2d21ce079
Evgeniy-D-2022 <evgeniyd1901@gmail.com>
2023-03-20 22:26:54 +0100

    end task-02-js

--- js/task-01.js
+++ js/task-01.js
@@ -4,4 +4,4 @@ console.log(`Number of categories: ${categoriesQuantity.length}`);
 const categoryQuantity = document.querySelector('ul#categories .item');
 
 categoriesQuantity.forEach(el => console.log(`Category: ${el.firstElementChild.textContent} 
-Elements:  ${el.lastElementChild.childElementCount}`));
+Elements:  ${el.lastElementChild.children.length}`));
