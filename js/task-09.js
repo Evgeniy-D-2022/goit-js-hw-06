4ef282a07e2cd557bbffd2eaabb65997f4e3175e
Evgeniy-D-2022 <evgeniyd1901@gmail.com>
2023-03-23 00:08:34 +0100

    update task-09-js

--- js/task-09.js
+++ js/task-09.js
@@ -1,5 +1,16 @@
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
-}
+const btnChangeColor = document.querySelector('.change-color');
+const bodyChangeBackground = document.body;
+const spanColor = document.querySelector('.color');
+
+btnChangeColor.addEventListener('click', btnClick);
+
+function btnClick() {
+  bodyChangeBackground.style.backgroundColor = getRandomHexColor();
+  spanColor.textContent = getRandomHexColor();
+
+  function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215)
+      .toString(16)
+      .padStart(6, 0)}`;
+  }
+};
