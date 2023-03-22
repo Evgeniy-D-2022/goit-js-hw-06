6d5acfc2152f4643d8fe39c165719bc6b41df27d
Evgeniy-D-2022 <evgeniyd1901@gmail.com>
2023-03-22 23:44:30 +0100

    update task-08-js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,25 @@
+const formLogin = document.querySelector('.login-form');
+
+formLogin.addEventListener('submit', onFormEvent);
+
+function onFormEvent(event) {
+    event.preventDefault();
+    const formEl = event.currentTarget.elements;
+    const formEmail = formEl.email;
+    const formPassword = formEl.password;
+    const formData = {
+        formEmail,
+        formPassword
+    };
+    console.log(formData);
+
+    if (formEmail.value === '' || formPassword.value === '') {
+        alert('All fields must be filled');
+    } else {
+        alert('The form submitted');
+        console.log(`Email: ${formEmail.value} and password ${formPassword.value}`);
+    }
+    event.currentTarget.reset();
+};
+
 
